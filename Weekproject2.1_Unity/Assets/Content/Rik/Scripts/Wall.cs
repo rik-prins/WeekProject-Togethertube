ae134114a6e11cfda0a3de4032b1a6632deab7dd
Rik <rik-prins@hotmail.com>
2020-02-04 14:31:40 +0100

    Oik

---
+++ Weekproject2.1_Unity/Assets/Content/Rik/Scripts/Wall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private void Start()
+    {
+        float cubeSize = Random.Range(1, 6);
+        int randomPick = Random.Range(0, 10);
+
+        if (randomPick == 1)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, cubeSize);
+        }
+    }
+}
---
+++ Weekproject2.1_Unity/Assets/Content/Rik/Scripts/WallSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    public GameObject[,] grid;
+    public GameObject gridTilePrefab;
+
+    private void Awake()
+    {
+        grid = new GameObject[10, 10];
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                GameObject prefab = null;
+
+                prefab = gridTilePrefab;
+
+                grid[y, x] = Instantiate(prefab, new Vector3(x, y), Quaternion.identity);
+            }
+        }
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
