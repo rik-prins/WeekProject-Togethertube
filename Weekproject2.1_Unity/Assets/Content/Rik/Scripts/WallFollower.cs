5735aa768a8aff2ffcd0a86165d88f29f39c7842
Rik <rik-prins@hotmail.com>
2020-02-05 12:53:34 +0100

    Wall Collider Follower sirrrrrrr

---
+++ Weekproject2.1_Unity/Assets/Content/Rik/Scripts/WallFollower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFollower : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
